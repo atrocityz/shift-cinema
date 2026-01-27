ab97a024d4a532c00eafeaac9fcbe3814fefa54a
atrocityz <disbalance17@mail.ru>
2026-01-27 23:30:53 +0700

    feat: setup tanstack react query and router

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      generatedRouteTree: './generated/router/index.ts',
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@/generated': path.resolve(__dirname, './generated'),
     },
   },
 })
