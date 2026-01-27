a8847e526537c765eb1b838eeb11a841bb57c15a
atrocityz <disbalance17@mail.ru>
2026-01-27 23:30:53 +0700

    feat: setup tanstack react query and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+
+import { routeTree } from '../generated/router'
+import { queryClient } from './utils/lib'
+
+const router = createRouter({
+  routeTree,
+  context: { queryClient },
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 export const App = () => {
-  return <div>123</div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }
