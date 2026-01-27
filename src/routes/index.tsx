5d8d6ce0ed88ede21da78e4490dae4b0918955e7
atrocityz <disbalance17@mail.ru>
2026-01-28 00:12:06 +0700

    feat: init shadcn ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Button } from '@/components/ui'
+
 const MainPage = () => {
-  return <div>MainPage</div>
+  return (
+    <div>
+      MainPage
+      <Button>123</Button>
+    </div>
+  )
 }
 
 export const Route = createFileRoute('/')({
