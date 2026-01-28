474ce204bf5a49466967fd217fadea177c0f22eb
atrocityz <disbalance17@mail.ru>
2026-01-29 03:32:44 +0700

    feat: add mobile nav

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,10 @@
 import type { ReactNode } from 'react'
 
-import { Link } from '@tanstack/react-router'
+import { Link, useMatchRoute } from '@tanstack/react-router'
 
 import { ExitIcon } from '../icons'
 import { Button } from '../ui'
+import { MobileBottomNav } from './components/MobileBottomNav'
 import { Navigation } from './components/Navigation'
 import { ThemeToggler } from './components/ThemeToggler'
 
@@ -11,27 +12,40 @@ interface LayoutProps {
   children: ReactNode
 }
 
-export const Layout = ({ children }: LayoutProps) => (
-  <div className="flex min-h-dvh flex-col">
-    <header className="hidden border-b border-gray-400 py-6 sm:block dark:border-gray-400/30">
-      <div className="container flex items-center gap-6">
-        <Link aria-label="Главная" className="shrink-0" title="Главная" to="/">
-          <img alt="ШИФТ Cinema" height={34} src="/logo.svg" width={118} />
-        </Link>
-        <Navigation />
-        <div className="ml-auto flex items-center gap-8">
-          <Button
-            className="p-0"
-            variant="ghost"
-            onClick={() => console.log('logout')}
+export const Layout = ({ children }: LayoutProps) => {
+  const matchRoute = useMatchRoute()
+  const isMainPage = matchRoute({ to: '/' })
+
+  return (
+    <div className="flex min-h-dvh flex-col">
+      <header className="hidden border-b border-gray-400 py-6 sm:block dark:border-gray-400/30">
+        <div className="container flex items-center gap-6">
+          <Link
+            aria-label="Главная"
+            className="shrink-0"
+            title="Главная"
+            to="/"
           >
-            <ExitIcon className="size-6" />
-            Выйти
-          </Button>
-          <ThemeToggler />
+            <img alt="ШИФТ Cinema" height={34} src="/logo.svg" width={118} />
+          </Link>
+          <Navigation />
+          <div className="ml-auto flex items-center gap-8">
+            <Button
+              className="p-0"
+              variant="ghost"
+              onClick={() => console.log('logout')}
+            >
+              <ExitIcon className="size-6" />
+              Выйти
+            </Button>
+            <ThemeToggler />
+          </div>
         </div>
-      </div>
-    </header>
-    <main className="container flex-1">{children}</main>
-  </div>
-)
+      </header>
+      <main className={`container flex-1 ${isMainPage ? 'pb-24 sm:pb-0' : ''}`}>
+        {children}
+      </main>
+      {isMainPage && <MobileBottomNav />}
+    </div>
+  )
+}
