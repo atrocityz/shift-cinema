1dc655ffe728e8fd063aee1260328cbde285f0ce
atrocityz <disbalance17@mail.ru>
2026-01-29 00:13:14 +0700

    feat: add film view

--- src/routes/films/$filmId.tsx
+++ src/routes/films/$filmId.tsx
@@ -1,8 +1,25 @@
-import { createFileRoute, useParams } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
+import { Loader } from 'lucide-react'
+
+import { FilmCard } from './-components/FilmCard.tsx'
+import { MoviePageNavigation } from './-components/MoviePageNavigation.tsx'
+import { useMoviePage } from './-hooks/useMoviePage.ts'
 
 const FilmPage = () => {
-  const { filmId } = useParams({ from: '/films/$filmId' })
-  return <div>{filmId}</div>
+  const { state } = useMoviePage()
+
+  return (
+    <div className="flex flex-col items-start gap-6 pb-6">
+      <MoviePageNavigation />
+      {state.loading && (
+        <Loader className="text-primary col-span-full mx-auto size-12 flex-1 animate-spin" />
+      )}
+      {!state.loading && !state.film && (
+        <p className="text-center text-2xl">Фильм не найден</p>
+      )}
+      {!state.loading && state.film && <FilmCard film={state.film} />}
+    </div>
+  )
 }
 
 export const Route = createFileRoute('/films/$filmId')({
