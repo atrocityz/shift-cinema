b3c9f095bf3c43f6b1d750927481c33c85ff211d
atrocityz <disbalance17@mail.ru>
2026-02-24 01:52:15 +0700

    refactor: improve form validation

--- src/routes/order/_layout/-components/steps/ProfileDataStep/schemas/profileDataFormSchema.ts
+++ src/routes/order/_layout/-components/steps/ProfileDataStep/schemas/profileDataFormSchema.ts
@@ -2,19 +2,35 @@ import { z } from 'zod'
 
 export const profileDataFormSchema = z.object({
   firstName: z
-    .string({ error: 'Имя должно быть строкой' })
-    .min(2, { message: 'Имя обязательно' }),
-  lastName: z
-    .string({ error: 'Фамилия должна быть строкой' })
-    .min(2, { message: 'Фамилия обязательна' }),
-  middleName: z
-    .string({ error: 'Отчество должно быть строкой' })
-    .min(2, { message: 'Отчество обязательно' }),
-  phoneNumber: z
-    .string({ error: 'Номер телефона должен быть строкой' })
-    .regex(/\d{10}$/, {
-      message: 'Неверно введен номер телефона',
+    .string({ error: `Имя обязательно` })
+    .trim()
+    .min(2, { error: `Имя должно быть не короче 2 символов` })
+    .max(50, { error: `Имя слишком длинное` })
+    .regex(/^[\p{L}\s-]+$/u, {
+      error: `Имя может содержать только буквы и дефис`,
     }),
+  lastName: z
+    .string({ error: `Фамилия обязательна` })
+    .trim()
+    .min(2, { error: `Фамилия должно быть не короче 2 символов` })
+    .max(50, { error: `Фамилия слишком длинная` })
+    .regex(/^[\p{L}\s-]+$/u, {
+      error: `Фамилия может содержать только буквы и дефис`,
+    }),
+
+  middleName: z
+    .string({ error: `Отчество обязательно` })
+    .trim()
+    .min(2, { error: `Отчество должно быть не короче 2 символов` })
+    .max(50, { error: `Отчество слишком длинное` })
+    .regex(/^[\p{L}\s-]+$/u, {
+      error: `Отчество может содержать только буквы и дефис`,
+    }),
+  phoneNumber: z
+    .string({ error: 'Введите номер телефона' })
+    .trim()
+    .min(1, { error: 'Поле обязательно для заполнения' })
+    .length(10, { error: 'Номер введен не полностью' }),
 })
 
 export type ProfileFormData = z.infer<typeof profileDataFormSchema>
